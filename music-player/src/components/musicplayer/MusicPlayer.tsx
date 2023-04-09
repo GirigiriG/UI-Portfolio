4f26829ba404f37622460498d7a94bf10b2369b6
Gideon Girigiri <gideongirigiri@gamil.com>
2023-04-09 18:35:36 -0400

    go auto play working on next and previous as well as loop song function was also able to get progress bar working

--- music-player/src/components/musicplayer/MusicPlayer.tsx
+++ music-player/src/components/musicplayer/MusicPlayer.tsx
@@ -10,13 +10,23 @@ import { BiRepeat } from "react-icons/bi";
 
 const MusicPlayer = () => {
    const [isPlaying, setIsPlaying] = useState<Boolean>(true);
-   const [currentSong, setCurrentSong] = useState<string>("evening.mp3");
+   const [tracks, _] = useState([
+      "Saucy_dog.mp3",
+      "evening.mp3",
+      "night-sky.mp3",
+      "KohanaLam.mp3",
+      "abandoned_house.mp3",
+   ]);
+   const [trackIndex, setTrackIndex] = useState<number>(0);
    const [currentTime, setCurrentTime] = useState<any>(0);
    const [songDuration, setSongDuration] = useState<any>(0);
+   const [repeatColor, setRepeatColor] = useState("a0a0a0");
+
    const audioElementRef = useRef<HTMLAudioElement>(null);
 
    const handleIsPlaying = () => {
       setSongDuration(audioElementRef.current?.duration);
+
       isPlaying === true
          ? audioElementRef.current?.play()
          : audioElementRef.current?.pause();
@@ -27,12 +37,65 @@ const MusicPlayer = () => {
    const setAudioTime = (audio: React.RefObject<HTMLAudioElement>) => {
       setInterval(() => {
          setCurrentTime(audio.current?.currentTime);
-      }, 1000);
+      }, 200);
    };
 
    const handleNextTrack = () => {
-      setCurrentSong("night-sky.mp3");
-      handleIsPlaying();
+      const currentIndex = trackIndex == tracks.length - 1 ? 0 : trackIndex + 1;
+      setTrackIndex(currentIndex);
+
+      setSongDuration(audioElementRef.current?.duration);
+      handlePlayPromise();
+   };
+
+   const handlePreviousTrack = () => {
+      setSongDuration(audioElementRef.current?.duration);
+
+      const currentIndex = trackIndex == tracks.length - 1 ? 0 : trackIndex - 1;
+      console.log(currentIndex);
+
+      if (currentIndex <= -1) {
+         setTrackIndex(tracks.length - 1);
+         handlePlayPromise();
+         return;
+      }
+
+      setTrackIndex(currentIndex);
+      handlePlayPromise();
+   };
+
+   const handleRepeat = () => {
+      setRepeatColor(repeatColor === "a0a0a0" ? "FFFFFF" : "a0a0a0");
+
+      if (audioElementRef.current?.loop != undefined) {
+         audioElementRef.current.loop = true;
+         // audioElementRef.current?.load();
+         handlePlayPromise();
+      }
+   };
+
+   const handlePlayPromise = () => {
+      const playPromise = audioElementRef.current?.play();
+      if (playPromise != undefined) {
+         playPromise
+            ?.then((_) => {
+               audioElementRef.current?.pause();
+               if (audioElementRef.current?.currentTime != undefined) {
+                  // audioElementRef.current.currentTime = 0;
+                  audioElementRef.current?.play();
+               }
+            })
+            .catch((err) => console.error(`error loading the video ${err}`));
+      }
+   };
+
+   const handleDrag = (event: React.ChangeEvent) => {
+      let input = event.target as HTMLInputElement
+      setCurrentTime(input.value);
+
+      if (audioElementRef.current?.currentTime != undefined) {
+         audioElementRef.current.currentTime = parseInt(input.value);
+      }
    };
 
    return (
@@ -40,13 +103,7 @@ const MusicPlayer = () => {
          <div className={styles.leftsection}>Left</div>
          <div className={styles.centersection}>
             <div className={styles.controls}>
-               <audio
-                  ref={audioElementRef}
-                  controls
-                  style={{ display: "none" }}
-               >
-                  <source src={currentSong} type="audio/mp3" />
-               </audio>
+               <audio ref={audioElementRef} src={tracks[trackIndex]}></audio>
                <div className={styles.shuffle}>
                   <BiShuffle
                      style={{ cursor: "poniter" }}
@@ -57,6 +114,7 @@ const MusicPlayer = () => {
                   <BiSkipPrevious
                      style={{ cursor: "poniter" }}
                      size={30}
+                     onClick={handlePreviousTrack}
                   ></BiSkipPrevious>
                </div>
                <div className={styles.play_pause} onClick={handleIsPlaying}>
@@ -74,24 +132,30 @@ const MusicPlayer = () => {
                      ></BiPause>
                   )}
                </div>
-               <div className={styles.next}>
+               <div className={styles.next} onClick={handleNextTrack}>
                   <BiSkipNext
                      style={{ cursor: "poniter" }}
                      size={30}
-                     onClick={handleNextTrack}
                   ></BiSkipNext>
                </div>
-               <div className={styles.repeat}>
-                  <BiRepeat style={{ cursor: "poniter" }} size={25}></BiRepeat>
+               <div className={styles.repeat} onClick={handleRepeat}>
+                  <BiRepeat
+                     style={{ cursor: "poniter" }}
+                     size={25}
+                     color={`#${repeatColor}`}
+                  ></BiRepeat>
                </div>
             </div>
             <div className={styles.progress}>
-               <progress
+               <input
+                  type="range"
                   className={styles.progress}
                   id="file"
                   value={currentTime}
+                  min={0}
                   max={songDuration}
-               ></progress>
+                  onChange={handleDrag}
+               />
             </div>
          </div>
          <div className={styles.rightsection}>Right</div>
