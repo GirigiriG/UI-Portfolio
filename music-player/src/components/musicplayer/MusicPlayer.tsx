40382c984e1bc92208b2e8baa1358d302e4ffa28
Gideon Girigiri <gideongirigiri@gamil.com>
2023-04-11 21:22:23 -0400

    deleted musicplayer.tsx by mistake

--- music-player/src/components/musicplayer/MusicPlayer.tsx
+++ music-player/src/components/musicplayer/MusicPlayer.tsx
@@ -10,6 +10,7 @@ import {
    BiRepeat,
 } from "react-icons/bi";
 import { FiVolume1, FiVolume2 } from "react-icons/fi";
+import { BsVolumeMute } from "react-icons/bs";
 
 const MusicPlayer = () => {
    const [isPlaying, setIsPlaying] = useState<Boolean>(true);
@@ -17,50 +18,91 @@ const MusicPlayer = () => {
       "Saucy_dog.mp3",
       "evening.mp3",
       "night-sky.mp3",
-      "KohanaLam.mp3",
+      "Salyu.mp3",
       "abandoned_house.mp3",
    ]);
    const [trackIndex, setTrackIndex] = useState<number>(0);
-   const [currentTime, setCurrentTime] = useState<any>(0);
+   const [currentTime, setCurrentTime] = useState<number>(0);
    const [volume, setVolume] = useState<number>(20);
    const [songDuration, setSongDuration] = useState<any>(0);
    const [repeatColor, setRepeatColor] = useState("a0a0a0");
+   const [songStartTime, setSongStartTime] = useState<string>("");
+   const [SongEndTime, setSongEndTime] = useState<String>("");
 
    const audioElementRef = useRef<HTMLAudioElement>(null);
 
    audioElementRef.current?.addEventListener("loadedmetadata", () => {
       setSongDuration(audioElementRef.current?.duration);
+      calculateSongDuration();
    });
+
+   audioElementRef.current?.addEventListener("timeupdate", (event) => {
+      calculatePlaytime();
+   });
+
+   const calculatePlaytime = () => {
+      let seconds: number = 0;
+      let minutes: number = 0;
+
+      if (audioElementRef.current?.currentTime != undefined) {
+         seconds = audioElementRef.current.currentTime % 60;
+         minutes = (audioElementRef.current.currentTime / 60) % 60;
+
+         if (seconds < 10) {
+            setSongStartTime(`${Math.trunc(minutes)}:0${Math.trunc(seconds)}`);
+            return;
+         }
+         setSongStartTime(`${Math.trunc(minutes)}:${Math.trunc(seconds + 1)}`);
+      }
+   };
+
+   const calculateSongDuration = () => {
+      if (audioElementRef.current?.duration != undefined) {
+         const duration = audioElementRef.current.duration;
+         const durationMinute: number = Math.floor(duration / 60);
+         const durationSecond: number = Math.floor(
+            duration - durationMinute * 60
+         );
+
+         setSongEndTime(`${durationMinute}:${durationSecond}`);
+      }
+   };
 
    const handleIsPlaying = () => {
       setSongDuration(audioElementRef.current?.duration);
+      calculateSongDuration();
+
 
       isPlaying === true
          ? audioElementRef.current?.play()
          : audioElementRef.current?.pause();
       setIsPlaying(!isPlaying);
-      setAudioTime(audioElementRef);
+      setAudioTime();
    };
 
-   const setAudioTime = (audio: React.RefObject<HTMLAudioElement>) => {
+   const setAudioTime = () => {
       setInterval(() => {
-         setCurrentTime(audio.current?.currentTime);
-      }, 10);
+         if (audioElementRef.current?.currentTime != undefined) {
+            setCurrentTime(audioElementRef.current.currentTime);
+         }
+      }, 100);
    };
 
    const handleNextTrack = () => {
       const currentIndex = trackIndex == tracks.length - 1 ? 0 : trackIndex + 1;
       setTrackIndex(currentIndex);
+      setIsPlaying(false);
 
       handlePlayPromise();
    };
 
    const handlePreviousTrack = () => {
       const currentIndex = trackIndex == tracks.length - 1 ? 0 : trackIndex - 1;
+      setIsPlaying(false);
 
       if (currentIndex <= -1) {
          setTrackIndex(tracks.length - 1);
-
+         setAudioTime();
          handlePlayPromise();
          return;
       }
@@ -78,23 +120,22 @@ const MusicPlayer = () => {
       }
    };
 
-   const handlePlayPromise = () => {
-      const playPromise = audioElementRef.current?.play();
-      if (playPromise != undefined) {
-         playPromise
-            ?.then((_) => {
-               audioElementRef.current?.pause();
-               if (audioElementRef.current?.currentTime != undefined) {
-                  audioElementRef.current?.play();
-               }
-            })
-            .catch((err) => console.error(`error loading the video ${err}`));
+   const handlePlayPromise = async () => {
+      await audioElementRef.current?.play();
+
+      try {
+         // audioElementRef.current?.pause();
+         if (audioElementRef.current?.currentTime != undefined) {
+            audioElementRef.current?.play();
+         }
+      } catch (error) {
+         console.error("Error occured ", error);
       }
    };
 
    const handleSeek = (event: React.ChangeEvent) => {
       let target = event.target as HTMLInputElement;
-      setCurrentTime(target.value);
+      setCurrentTime(parseInt(target.value));
 
       if (audioElementRef.current?.currentTime != undefined) {
          audioElementRef.current.currentTime = parseInt(target.value);
@@ -105,8 +146,23 @@ const MusicPlayer = () => {
       let target = event.target as HTMLInputElement;
       setVolume(parseInt(target.value));
 
+      if (
+         parseInt(target.value) === 0 &&
+         audioElementRef.current?.muted != undefined
+      ) {
+         audioElementRef.current.muted = true;
+         return;
+      }
+
       if (audioElementRef.current?.volume != undefined) {
+         audioElementRef.current.muted = false;
          audioElementRef.current.volume = volume / 100;
+      }
+   };
+
+   const handleSpaceKeyPress = (event: React.KeyboardEvent<Element>) => {
+      if (event.keyCode === 32) {
+         handleIsPlaying();
       }
    };
 
@@ -132,7 +188,7 @@ const MusicPlayer = () => {
                <div className={styles.play_pause} onClick={handleIsPlaying}>
                   {isPlaying === true ? (
                      <BiPlay
-                        style={{ cursor: "poniter" }}
+                        style={{ cursor: "poniter", marginLeft: "2px" }}
                         size={30}
                         color={"black"}
                      ></BiPlay>
@@ -159,7 +215,8 @@ const MusicPlayer = () => {
                </div>
             </div>
             <div className={styles.progress}>
-            {/* <span>{(parseInt(Math.floor(currentTime).toPrecision(2)) / 60) * 100}</span> */}
+               <span>{songStartTime}</span>
+               {/* <h2>{songDuration}</h2> */}
                <input
                   type="range"
                   className={styles.music_slider}
@@ -167,9 +224,11 @@ const MusicPlayer = () => {
                   value={currentTime}
                   min={0}
                   max={songDuration}
+                  onKeyDown={handleSpaceKeyPress}
                   onChange={handleSeek}
                />
-               {/* <span>{(parseInt(Math.floor(songDuration).toPrecision(2)) / 60) * 100}</span> */}
+               {/* <h2>{currentTime}</h2> */}
+               <span>{SongEndTime}</span>
             </div>
          </div>
          <div className={styles.rightsection}>
